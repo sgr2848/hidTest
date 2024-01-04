de40d681ae54f6c23107ef2e358730ac1a7211b3
sgr2848 <sg28r8@gmail.com>
2024-01-04 10:23:38 +0545

    Panic and race conditions handled

--- rfid_messenger/main.go
+++ rfid_messenger/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/ebfe/scard"
@@ -14,21 +15,12 @@ import (
 	"github.com/sstallion/go-hid"
 )
 
-var clients = make(map[*websocket.Conn]bool)
-var broadcast = make(chan []byte)
+var (
+	clients   = make(map[*websocket.Conn]bool)
+	broadcast = make(chan []byte)
+	mutex     = sync.Mutex{}
+)
 
-func parseRFIDDataA(data []byte) {
-	// Check if the length of the data buffer is three bytes
-	if len(data) != 3 {
-		fmt.Println("Invalid data length. Expected 3 bytes.")
-		return
-	}
-
-	// Assuming the data represents a numeric value encoded in the three bytes
-	value := int(data[0])*100 + int(data[1])*10 + int(data[2])
-
-	fmt.Printf("Parsed RFID value: %d\n", value)
-}
 func parseRFIDData(buffers [][3]byte) {
 	//much wow
 	digitMapping := map[[3]byte]int{
@@ -47,79 +39,70 @@ func parseRFIDData(buffers [][3]byte) {
 	var result strings.Builder
 
 	for _, buf := range buffers {
-		// Check if the buffer exists in the mapping and is not to be ignored
 		if digit, ok := digitMapping[buf]; ok && buf != [3]byte{2, 0, 0} && buf != [3]byte{2, 0, 40} {
 			// Append the digit to the result
 			result.WriteString(strconv.Itoa(digit))
 		}
 	}
-
 	finalResult := result.String()
+	mutex.Lock()
 	broadcast <- []byte(finalResult)
+	mutex.Unlock()
 	// fmt.Println("Extracted RFID number:", finalResult)
 }
 
-func hidMake() {
+func handleHidEvents() {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Recovered from panic:", r)
+		}
+	}()
 	err := hid.Init()
 	if err != nil {
 		log.Println("Error")
 	}
 	var rfidReaderVid uint16 = 0
 	var rfidReaderPid uint16 = 0
+	for {
+		hid.Enumerate(hid.VendorIDAny, hid.ProductIDAny, func(info *hid.DeviceInfo) error {
 
-	hid.Enumerate(hid.VendorIDAny, hid.ProductIDAny, func(info *hid.DeviceInfo) error {
-
-		if strings.Contains(strings.ToLower(info.ProductStr), "rfid") {
-			log.Println("Found RFID Reader")
-			log.Println(info.BusType)
-			log.Println(info.UsagePage)
-			rfidReaderVid = info.VendorID
-			rfidReaderPid = info.ProductID
-
-			log.Println("PID: ", rfidReaderPid)
-			log.Println("VID: ", rfidReaderVid)
-		}
-		return nil
-	})
-	log.Println()
-	if rfidReaderVid == 0 && rfidReaderPid == 0 {
-		log.Println("No RFID Reader Found")
-	} else {
-		dev, err := hid.OpenFirst(rfidReaderVid, rfidReaderPid)
-		if err != nil {
-			log.Println(err)
-			log.Println("Failed to open")
-			log.Println("Error opening device")
-			return
-		}
-		// defer dev.Close()
-		// dev.SetNonblock(true)
-		count := 0
-		var buffers [][3]byte
-		for {
-			buf := make([]byte, 3)
-			n, err := dev.Read(buf)
-			if err != nil {
-				log.Println("Error reading device:", err)
+			if strings.Contains(strings.ToLower(info.ProductStr), "rfid") {
+				rfidReaderVid = info.VendorID
+				rfidReaderPid = info.ProductID
 			}
-			if n > 0 {
-				// Convert bytes to hexadecimal string
-				// hexString := hex.EncodeToString(buf[:n])
-				// fmt.Println("Read Hex:", hexString)
-				// broadcast <- []byte(hexString)
+			return nil
+		})
+		if rfidReaderVid == 0 && rfidReaderPid == 0 {
 
-				log.Println("count read:", count)
-				count++
-				buffers = append(buffers, [3]byte{buf[0], buf[1], buf[2]})
-				if len(buffers) == 26 {
-					log.Println("buffers:", buffers)
-					parseRFIDData(buffers)
-					buffers = nil
+		} else {
+			dev, err := hid.OpenFirst(rfidReaderVid, rfidReaderPid)
+			if err != nil {
+				return
+			}
+			// defer dev.Close()
+			// dev.SetNonblock(true)
+			count := 0
+			var buffers [][3]byte
+			for {
+				buf := make([]byte, 3)
+				n, err := dev.Read(buf)
+				if err != nil {
+					log.Println("Error reading:", err)
+					break
+				}
+				if n > 0 {
+					count++
+					buffers = append(buffers, [3]byte{buf[0], buf[1], buf[2]})
+					if len(buffers) == 26 {
+						log.Println("buffers:", buffers)
+						parseRFIDData(buffers)
+						buffers = nil
 
+					}
 				}
 			}
 		}
-
+		time.Sleep(2 * time.Second)
 	}
 }
 func main() {
@@ -129,11 +112,12 @@ func main() {
 		fmt.Println("Error decoding hexadecimal string:", err)
 		return
 	}
-	go hidMake()
-
+	go handleHidEvents()
 	http.HandleFunc("/", handleWebSocket)
 	go startWebSocketServer()
-
+	handleSmartCard()
+}
+func handleSmartCard() {
 	// Continue with the smart card listener as before
 	ctx, err := scard.EstablishContext()
 	if err != nil {
@@ -144,22 +128,22 @@ func main() {
 	var lastUID []byte
 
 	// Create a goroutine to handle broadcasting UID data to WebSocket clients
-	go func() {
-		for {
-			uid := <-broadcast
-			if uid == nil {
-				continue
-			} else {
-				for client := range clients {
-					time.Sleep(1 * time.Second)
-					err := client.WriteMessage(websocket.TextMessage, uid)
-					if err != nil {
-						delete(clients, client)
-					}
-				}
-			}
-		}
-	}()
+	// go func() {
+	// 	for {
+	// 		uid := <-broadcast
+	// 		if uid == nil {
+	// 			continue
+	// 		} else {
+	// 			for client := range clients {
+	// 				time.Sleep(1 * time.Second)
+	// 				err := client.WriteMessage(websocket.TextMessage, uid)
+	// 				if err != nil {
+	// 					delete(clients, client)
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }()
 
 	for {
 		// List available readers
@@ -171,59 +155,66 @@ func main() {
 		}
 
 		if len(readers) == 0 {
-			// broadcast <- []byte{110, 111, 95, 114, 101, 97, 100, 101, 114}
-
+			mutex.Lock()
+			broadcast <- []byte{110, 111, 95, 114, 101, 97, 100, 101, 114}
+			mutex.Unlock()
 			// Wait for a while before trying again
 			time.Sleep(1 * time.Second)
-			continue
+
 		}
-
-		// Connect to the first reader (if available)
-		reader, err := ctx.Connect(readers[0], scard.ShareShared, scard.ProtocolAny)
-		if err != nil {
-			// broadcast <- []byte{99, 111, 110, 110, 101, 99, 116, 95, 102, 97, 105, 108, 101, 100}
-
-			time.Sleep(2 * time.Second)
-			continue
-		}
-
-		fmt.Println("Connected to the reader:", readers[0])
-
-		defer reader.Disconnect(scard.LeaveCard)
-
-		// Reader connected, start listening for card presence
-		for {
-			// Check for card presence
-			_, err := reader.Status()
-			if err == scard.ErrNoSmartcard {
-				// No card present, wait and continue listening
-				if lastUID != nil {
-					broadcast <- lastUID // Send the last received UID
-				}
-				continue
-			} else if err != nil {
-
-				time.Sleep(1 * time.Second)
-				break
-			}
-			apduCommand := []byte{0xFF, 0xCA, 0x00, 0x00, 0x00}
-			response, err := reader.Transmit(apduCommand)
+		if len(readers) > 0 {
+			// Connect to the first reader (if available)
+			reader, err := ctx.Connect(readers[0], scard.ShareShared, scard.ProtocolAny)
 			if err != nil {
-
-				time.Sleep(1 * time.Second)
-				break
+				mutex.Lock()
+				broadcast <- []byte{99, 111, 110, 110, 101, 99, 116, 95, 102, 97, 105, 108, 101, 100}
+				mutex.Unlock()
+				time.Sleep(2 * time.Second)
+				continue
 			}
 
-			// Process the response data to extract the UID
-			if len(response) >= 2 && response[len(response)-2] == 0x90 && response[len(response)-1] == 0x00 {
-				uid := response[:len(response)-2]
-				fmt.Printf("UID: %X\n", uid)
-				broadcast <- []byte(hex.EncodeToString(uid))
+			fmt.Println("Connected to the reader:", readers[0])
 
-			} else {
-				fmt.Println("Error: Unable to retrieve UID")
+			defer reader.Disconnect(scard.LeaveCard)
+
+			// Reader connected, start listening for card presence
+			for {
+				// Check for card presence
+				_, err := reader.Status()
+				if err == scard.ErrNoSmartcard {
+					// No card present, wait and continue listening
+					if lastUID != nil {
+						mutex.Lock()
+						broadcast <- lastUID // Send the last received UID
+						mutex.Unlock()
+					}
+					continue
+				} else if err != nil {
+
+					time.Sleep(1 * time.Second)
+					break
+				}
+				apduCommand := []byte{0xFF, 0xCA, 0x00, 0x00, 0x00}
+				response, err := reader.Transmit(apduCommand)
+				if err != nil {
+
+					time.Sleep(1 * time.Second)
+					break
+				}
+
+				// Process the response data to extract the UID
+				if len(response) >= 2 && response[len(response)-2] == 0x90 && response[len(response)-1] == 0x00 {
+					uid := response[:len(response)-2]
+					mutex.Lock()
+					broadcast <- []byte(hex.EncodeToString(uid))
+					mutex.Unlock()
+
+				} else {
+					fmt.Println("Error: Unable to retrieve UID")
+				}
 			}
 		}
+		time.Sleep(1 * time.Second)
 	}
 }
 
@@ -234,7 +225,7 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println("Failed to upgrade connection:", err)
+		http.Error(w, "Failed to upgrade connection to WebSocket", http.StatusInternalServerError)
 		return
 	}
 
@@ -249,24 +240,24 @@ func startWebSocketServer() {
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		socket, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
-			log.Println("Failed to upgrade connection:", err)
 			return
 		}
 		defer socket.Close()
 
-		clients[socket] = true
-
 		for {
 			uid := <-broadcast
-			log.Println("Send to client:", uid)
-			err := socket.WriteMessage(websocket.BinaryMessage, uid) // Send binary data as binary frame
+			log.Println(uid)
+
+			err := socket.WriteMessage(websocket.TextMessage, uid) // Send binary data as binary frame
 			if err != nil {
-				log.Println("Failed to send UID to WebSocket client:", err)
 				delete(clients, socket)
 				break
 			}
 		}
 	})
 
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Failed to start WebSocket server:", err)
+	}
 }
